e1eb849f2d7f8f73e26e301e5617a304cf930ad2
InfiniteProductions <justice8@bbox.fr>
2023-03-02 17:18:54 +0100

    main menu improved
    - all buttons handler work (many are still place holder)

--- project/Scripts/main_menu.gd
+++ project/Scripts/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 func _ready():
+	Global.debug_enabled = true #tmp
 	Global.debug("MainMenu: generate tile stripe")
 	
 	var tile : Node2D = null
@@ -58,4 +59,33 @@ func _ready():
 
 
 func _on_help_item_help_requested():
-	pass # Replace with function body.
+	Global.debug("help button clicked")
+	#$VBoxContainer.hide()
+
+	$VBoxContainer/MarginContainer.hide()
+	$VBoxContainer/MarginContainer2.hide()
+	$HelpPanel.show()
+
+func _on_settings_item_settings_requested():
+	Global.debug("Settings button clicked")
+
+func _on_about_item_about_requested():
+	Global.debug("About button clicked")
+
+func _on_quit_item_quit_requested():
+	Global.debug("Quit button clicked")
+	get_tree().quit()
+
+func _on_one_player_item_oneplayer_game_requested():
+	Global.debug("1P game button clicked")
+
+func _on_two_players_item_twoplayers_game_requested():
+	Global.debug("2P game button clicked")
+
+func _on_high_scores_item_highscores_requested():
+	Global.debug("HighSCores button clicked")
+
+func _on_help_panel_help_panel_closed():
+	#$VBoxContainer.show()
+	$VBoxContainer/MarginContainer.show()
+	$VBoxContainer/MarginContainer2.show()
