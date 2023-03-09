e63659601b099e0d7a9538dd8d54659de973a7c5
InfiniteProductions <justice8@bbox.fr>
2023-03-09 18:47:00 +0100

    Configuration and highscores added
    -add data structure for settings and highscores
    -add config load/save helper functions
    -early highscores management functions added

--- project/Scripts/Global.gd
+++ project/Scripts/Global.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var debug_enabled : bool
 
+const SETTINGS_FILE_PATH = "user://settings.ini"
+const HIGHSCORES_FILE_PATH = "user://highscores.dat"
+
 var stats : Dictionary = {
 	"GamesPlayed": 0,
 	"GamesWon": 0,
@@ -11,6 +14,29 @@ var stats : Dictionary = {
 	"TotalFourWays": 0,
 	"TotalThreeWays": 0,
 	"TotalTwoWays": 0
+}
+
+var settings : Dictionary = {
+	"Audio": { 
+		"soundEffects": true,
+		"soundVolume": 10,
+		"music": true,
+		"musicVolume": 5,
+		"musicToPlay": 0
+	},
+	"Display": {
+		"screenOrientation": 0,
+		"screenResolution": "TODO",
+		"fullScreen": false
+	},
+	"hints": 0,
+	"language": "TODO"
+}
+
+var highScores : Dictionary = {
+	"OnePlayer" : [],
+	"TwoPlayers" : [],
+	"Tournament": []
 }
 
 var avail_tile_shapes = [
@@ -36,8 +62,73 @@ enum ButtonIDs { BUTTON_1PGAME, BUTTON_2PGAME, BUTTON_TOURNAMENT, BUTTON_HELP, B
 var tile_colors : int = 6
 var tile_shapes : int = 6
 
-var highscore_file = "user://highscores.dat"
-var settings_file = "user://settings.dat"
+
+func save_config():
+	var config = ConfigFile.new()
+	
+	for item in settings:
+		Global.debug("item=["+item+"]")
+		if settings[item] is Dictionary:
+			for subItem in settings[item]:
+				Global.debug("subItem=["+subItem+"]")
+				config.set_value(item, subItem, settings[item][subItem])
+		else:
+			config.set_value("misc", item, settings[item])
+	
+	var error = config.save(SETTINGS_FILE_PATH)
+	if error != OK:
+		Global.debug("error=["+error+"]") #TODO: deal with eror
+
+
+func load_config():
+	var config = ConfigFile.new()
+	var error = config.load(SETTINGS_FILE_PATH)
+	if error != OK:
+		Global.debug("error=["+error+"]") #TODO: deal with eror
+	else:
+		var cfgval
+	
+		for item in settings:
+			Global.debug("item=["+item+"]")
+			if settings[item] is Dictionary:
+				for subItem in settings[item]:
+					Global.debug("subItem=["+subItem+"]")
+					if config.has_section(item):
+						if config.has_section_key(item, subItem):
+							cfgval = config.get_value(item, subItem, settings[item][subItem])
+							Global.debug("cfgloaded ("+item+"/"+subItem+")=["+str(cfgval)+"]")
+			else:
+				cfgval = config.get_value("misc", item, settings[item])
+				Global.debug("cfgloaded ("+item+")=["+str(cfgval)+"]")
+
+
+func initialize_high_scores():
+	for gameType in highScores:
+		highScores[gameType] = []
+		for idx in randi_range(0, 9):
+			highScores[gameType][idx] = [0, 0, 0]
+			#score from 10 to 100 by 10, rem tiles from 32 to 0 by 4
+			# 4w: 1 to 3
+
+
+func load_high_scores():
+	var highscores = ConfigFile.new()
+	var error = highscores.load(HIGHSCORES_FILE_PATH)
+	if error != OK:
+		Global.debug("error=["+error+"]") #TODO: deal with eror
+	else:
+		pass
+
+
+func save_high_scores():
+	var highscores = ConfigFile.new()
+	
+	#TODO: save data
+
+	var error = highscores.save(HIGHSCORES_FILE_PATH)
+	if error != OK:
+		Global.debug("error=["+error+"]") #TODO: deal with eror
+
 
 func debug(msg : String) -> void:
 	if (debug_enabled == true):
