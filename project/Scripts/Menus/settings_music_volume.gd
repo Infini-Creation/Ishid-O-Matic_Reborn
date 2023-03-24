d6b1badfea491a73504cd6016324416d24c25864
InfiniteProductions <justice8@bbox.fr>
2023-03-24 22:43:43 +0100

    Settings item improved
    sound and music button + volume

--- project/Scripts/Menus/settings_music_volume.gd
+++ project/Scripts/Menus/settings_music_volume.gd
@@ -1,8 +1,36 @@
 extends Control
 
 # use global to adjust volume and/or enable/disable music
+@onready var VolumeValue = $"HBoxContainer/Volume Value"
+
+var music_volume : int
+var music_enabled : bool
+
+signal settings_updated(int, bool)
+
+func _ready():
+	#init from settings, here fake sample data for testing
+	#music_volume = 60
+	#music_enabled = true
+	
+	$HBoxContainer/HSlider.value = music_volume
+	$HBoxContainer/TextureButton.button_pressed = !music_enabled
+	
+	VolumeValue.text = str($HBoxContainer/HSlider.value)
+
+
+func load_settings(updated_music_volume : int, updated_music_enabled : bool) -> void:
+	if updated_music_volume >= 0 and updated_music_volume <= 100:
+		music_volume = updated_music_volume
+	music_enabled = updated_music_enabled
 
 
 func _on_texture_button_toggled(button_pressed):
 	print("butt press="+str(button_pressed))
+	$HBoxContainer/HSlider.editable = !button_pressed
 	#bp=true = music disabled, false = enabled
+	settings_updated.emit(music_volume, !button_pressed)
+
+func _on_h_slider_value_changed(value):
+	VolumeValue.text = str(value)
+	settings_updated.emit(value, music_enabled)
--- project/Scripts/Menus/settings_sound_volume.gd
+++ project/Scripts/Menus/settings_sound_volume.gd
@@ -1,7 +1,37 @@
 extends Control
 
-# use global to adjust volume and/or enable/disable music
+# use global to adjust volume and/or enable/disable sound
+@onready var VolumeValue = $"HBoxContainer/Volume Value"
+
+var sound_volume : int
+var sound_enabled : bool
+
+signal settings_updated(int, bool)
+
+func _ready():
+	#init from settings, here fake sample data for testing
+	#sound_volume = 12
+	#sound_enabled = false
+	
+	$HBoxContainer/HSlider.value = sound_volume
+	$HBoxContainer/TextureButton.button_pressed = !sound_enabled
+	
+	VolumeValue.text = str($HBoxContainer/HSlider.value)
+
+
+func load_settings(updated_sound_volume : int, updated_sound_enabled : bool) -> void:
+	if updated_sound_volume >= 0 and updated_sound_volume <= 100:
+		sound_volume = updated_sound_volume
+	sound_enabled = updated_sound_enabled
+	
 
 
 func _on_texture_button_toggled(button_pressed):
-	pass # Replace with function body.
+	print("butt press="+str(button_pressed))
+	$HBoxContainer/HSlider.editable = !button_pressed
+	#bp=true = sound disabled, false = enabled !!=> depend on initial state !!
+	settings_updated.emit(sound_volume, !button_pressed)
+
+func _on_h_slider_value_changed(value):
+	VolumeValue.text = str(value)
+	settings_updated.emit(value, sound_enabled)
