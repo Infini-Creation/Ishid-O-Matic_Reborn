8b75f0124b9a4c162c8b0b4096c965e9b8077a60
InfiniteProductions <justice8@bbox.fr>
2023-03-25 13:23:04 +0100

    About panel updated
    replace Label by RichTextLabel with some text effects, color and links,
    add code to handle hyperlink

--- project/Scripts/Menus/about_panel.gd
+++ project/Scripts/Menus/about_panel.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_texture_button_pressed():
 	hide()
+
+#WIP
+func _on_rich_text_label_meta_clicked(meta):
+	var err = OS.shell_open(meta)
+	if err == OK:
+		print("Opened link '%s' successfully!" % meta)
+	else:
+		print("Failed opening the link '%s'!" % meta)
