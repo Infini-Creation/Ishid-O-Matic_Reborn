46711bebb0c887ee9eb67d25dcbd4a7b5a9e1dc8
InfiniteProductions <justice8@bbox.fr>
2023-03-28 10:21:27 +0200

    Highscores panel improved

--- project/Scripts/Menus/high_scores_panel.gd
+++ project/Scripts/Menus/high_scores_panel.gd
@@ -2,17 +2,28 @@ extends Control
 
 #add pages count, like help prev/next or click = next then close
 @export var scoreItemScene : PackedScene = preload("res://Scenes/HighScores/ScoreItem.tscn")
+var currentPage : String
 
 
 func _ready():
-	#add dummy entries
-	var node : Control
+	var node
 	
-	for idx in range(0,2):
+	#read global highscores table
+	
+	for idx in range(0,10):
 		node = scoreItemScene.instantiate()
+		node.init(idx+1, "aaa", 10, 10, 10)
 		$"CenterContainer/VBoxContainer/HighScores-Items".add_child(node)
 	
 	pass
 
 func _on_texture_button_pressed():
 	hide()
+
+
+func _on_previous_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_next_button_pressed():
+	pass # Replace with function body.
