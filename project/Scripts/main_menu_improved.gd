6423e4de70d77c471e3beadb41e4464854767306
InfiniteProductions <justice8@bbox.fr>
2023-04-21 08:42:26 +0200

    Update main_menu_improved.gd
    -add launching the game using related buttons
    -update to match new tile design

--- project/Scripts/main_menu_improved.gd
+++ project/Scripts/main_menu_improved.gd
@@ -3,10 +3,15 @@ extends Control
 @onready var MainMenuButtons : PackedScene = preload("res://Scenes/Main Menu/Buttons.tscn")
 @onready var SettingsButtons : PackedScene = preload("res://Scenes/SettingsMenu/Controls.tscn")
 @onready var highScoresPage : PackedScene = preload("res://Scenes/high_scores.tscn")
+@onready var gameScene : PackedScene = preload("res://Scenes/game.tscn")
 
 var mainMenuButtons
 var settingsMenu
 var highScores
+var game
+var mainMenuSave : Node
+
+signal launch_game(game_type : String)
 
 func _ready():
 	#Global.debug_enabled = true #tmp
@@ -24,14 +29,16 @@ func _ready():
 
 	for shape in Global.avail_tile_shapes:
 		for color in Global.avail_tile_colors:
-			tile = shape.instantiate()
-			tile.modulate = Global.avail_tile_colors[color]
+			tile = Global.avail_tile_shapes[shape].instantiate()
+			tile.get_node("tile symbol").modulate = Global.avail_tile_colors[color]
 			tile.color = color
 			tilesDeck[deckIdx] = tile
 			deckIdx += 1
 
 	tilesDeck.shuffle()
 	Global.debug("td="+str(tilesDeck.size()))
+	
+	game = gameScene.instantiate()
 	
 	var idx = 0
 	while idx <= 32:
@@ -62,11 +69,25 @@ func _on_button_click_received(buttonID : int):
 	# Main menu buttons
 		Global.ButtonIDs.BUTTON_1PGAME:
 			#setup main game and run it
-			pass
+			#get_tree().get_root().print_tree()
+			#game.print_tree()
+			#get_node("MainMenu").hide()
+			# maybe add a dummy root node to main menu & game scene
+			# remove/add child of this node
+			#mainMenuSave = get_node(".")
+			##get_tree().get_root().remove_child(get_node("."))
+			#self.print_tree()
+			##get_tree().get_root().add_child(game)
+			launch_game.emit(Global.GAMETYPE_ONEPLAYER)
+			
+			#game.setup(1, Global.GAMETYPE_ONEPLAYER)
 		Global.ButtonIDs.BUTTON_2PGAME:
-			pass
+			launch_game.emit(Global.GAMETYPE_TWOPLAYERS)
+			#game.setup(2, Global.GAMETYPE_TWOPLAYERS)
 		Global.ButtonIDs.BUTTON_TOURNAMENT:
-			pass #TODO
+			launch_game.emit(Global.GAMETYPE_TOURNAMENT)
+			#game.setup(?, Global.GAMETYPE_TOURNAMENT)
+			#pass #TODO
 			#display a special Coming Soon panel for this
 		Global.ButtonIDs.BUTTON_ABOUT:
 			$AboutPanel.show()
@@ -101,6 +122,7 @@ func _on_button_click_received(buttonID : int):
 			Global.debug("call save settings HERE")
 			# add feedback settings save successfully (small popup label)
 			
+			# + update sound volume and on/off status
 			# gather settings (signal ?) or save in settings script directly
 			#  would be better no need to transfert data here, global is global !
 			# as above, go back to main menu ?
