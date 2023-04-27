0b74cb55a4159b47d60c5fb36939c7975cef837c
InfiniteProductions <justice8@bbox.fr>
2023-04-27 10:55:08 +0200

    about panel modified for hyperlink
    attempt to make hyperlink click first processed before big background texture button get the click event and close it

--- project/Scripts/Menus/about_panel.gd
+++ project/Scripts/Menus/about_panel.gd
@@ -1,13 +1,26 @@
 extends Control
 
+var linkOpened : bool = false
 
 func _on_texture_button_pressed():
-	hide()
+	print("textbutton pressed")
+	if !linkOpened:
+		print("hide panel")
+		hide()
 
-#WIP
+
 func _on_rich_text_label_meta_clicked(meta):
 	var err = OS.shell_open(meta)
 	if err == OK:
 		print("Opened link '%s' successfully!" % meta)
+		linkOpened = true
 	else:
 		print("Failed opening the link '%s'!" % meta)
+	linkOpened = false
+
+
+func _on_action_button_pressed():
+	print("textbutton pressed lo="+str(linkOpened))
+	if !linkOpened:
+		print("hide panel")
+		hide()
