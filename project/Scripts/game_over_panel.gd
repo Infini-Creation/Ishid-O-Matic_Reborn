0b7a6917956b5fec24d99bd09bd01700b4afdb12
InfiniteProductions <justice8@bbox.fr>
2023-04-27 10:54:02 +0200

    Update game_over_panel.gd
    -useless _ready function removed
    -always show the panel (happen only on loss)
    -add dummy scene transition

--- project/Scripts/game_over_panel.gd
+++ project/Scripts/game_over_panel.gd
@@ -1,23 +1,28 @@
 extends Control
 
-func _ready():
-	print("GameOverPanel: _ready call")
+#func _ready():
+	#print("GameOverPanel: _ready call")
 
 
-func _on_game_game_end(win : bool):
-	print("GameOverPanel: game end signal received: w="+str(win))
-	if (win == false):
-		show()
-		#get_tree().paused = true
-	else:
-		# maybe display a game win panel and/or some festive animations
-		#display highscore panel & register name + save => just emit button pressed or doing same as below
-		pass
+func _on_game_game_is_over():
+	print("GameOverPanel: game over signal received")
+	
+	show()
+	#get_tree().paused = true
+
+	# maybe display a game win panel and/or some festive animations
+	#display highscore panel & register name + save => just emit button pressed or doing same as below
+
 
 
 func _on_texture_button_pressed():
-	print("GameOverPanel: button pressed, go to highscores panel IF hs beat or main menu")
+	print("GameOverPanel: button pressed, go back to main menu")
+	#signal back game to exit ?
+	# maybe: scene transition
+	get_tree().change_scene_to_file(Global.previous_scene)
+	
+	#not here
 	#TODO: check highscores if one > lowest at least go to page
 	#TODO: put a new highscore line where it belongs
-	#TODO: ask name then update highscores data
+	#TODO: (ask name then update highscores data) = use defined name
 	#TODO: go to main menu
