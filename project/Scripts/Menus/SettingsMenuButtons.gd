9846c68af8fbf181c9eefbb512ea7e3ca082c837
InfiniteProductions <justice8@bbox.fr>
2023-04-28 10:45:42 +0200

    Update SettingsMenuButtons.gd
    -add highlight mode setting
    -add language setting (still WIP)
    -remove unused/unneeded functions

--- project/Scripts/Menus/SettingsMenuButtons.gd
+++ project/Scripts/Menus/SettingsMenuButtons.gd
@@ -7,6 +7,15 @@ var music_enabled : bool
 var sound_enabled : bool
 var player1_name : String
 var player2_name : String
+var highlight_mode : int
+var language : int
+
+@onready var languageButton = [
+	"ButtonsGroup/Hints-Language Row/Language-0",
+	"ButtonsGroup/Hints-Language Row/Language-1",
+	"ButtonsGroup/Hints-Language Row/Language-2"
+]
+
 
 func _ready():
 	Global.debug_enabled = true #tmp
@@ -20,13 +29,18 @@ func _ready():
 	sound_volume = Global.settings["Audio"]["soundVolume"]
 	player1_name = Global.settings["Players"]["One"]
 	player2_name = Global.settings["Players"]["Two"]
-	
+	highlight_mode = Global.settings["hints"]
+	language = Global.settings["language"]
+
 	#init settings
 	Global.debug("cmv="+str(music_volume)+" cme="+str(music_enabled))
 	$"ButtonsGroup/Music Row/AudioVolume".load_settings(music_volume, music_enabled)
 	$"ButtonsGroup/Sound Row/AudioVolume".load_settings(sound_volume, sound_enabled)
 	$"ButtonsGroup/Music Row/settings_player1_name".load_settings(player1_name)
 	$"ButtonsGroup/Sound Row/settings_player2_name".load_settings(player2_name)
+	$"ButtonsGroup/Hints-Language Row/HighlightMode".load_settings(highlight_mode)
+	Global.debug("hlm="+str(highlight_mode)+" lang="+str(language))
+	#init language button (selected = pressed)
 
 func _on_accept_item_settings_save_requested():
 	Global.debug("save settings clicked")
@@ -41,6 +55,8 @@ func _on_accept_item_settings_save_requested():
 	Global.settings["Audio"]["soundVolume"] = sound_volume
 	Global.settings["Players"]["One"] = player1_name
 	Global.settings["Players"]["Two"] = player2_name
+	Global.settings["hints"] = highlight_mode
+	Global.settings["language"] = language
 	Global.save_config()
 	
 	button_clicked.emit(Global.ButtonIDs.BUTTON_SETTINGS_SAVE)
@@ -49,20 +65,6 @@ func _on_back_item_settings_revert_requested():
 	Global.debug("back clicked")
 	button_clicked.emit(Global.ButtonIDs.BUTTON_SETTINGS_BACK)
 
-#??
-func _on_quit_item_quit_requested():
-	pass # Replace with function body.
-
-func _on_music_volume_settings_updated(new_music_volume, new_music_enabled):
-	Global.debug("music settings: vol="+str(new_music_volume)+" on/off="+str(new_music_enabled))
-	music_volume = new_music_volume
-	music_enabled = new_music_enabled
-
-func _on_sound_volume_settings_updated(new_sound_volume, new_sound_enabled):
-	#Global.debug("sound settings: vol="+str(new_sound_volume)+" on/off="+str(new_sound_enabled))
-	sound_volume = new_sound_volume
-	sound_enabled = new_sound_enabled
-
 func _on_settings_player_1_name_settings_updated(new_player_name):
 	Global.debug("new p1 name="+new_player_name)
 	player1_name = new_player_name
@@ -70,7 +72,6 @@ func _on_settings_player_1_name_settings_updated(new_player_name):
 func _on_settings_player_2_name_settings_updated(new_player_name):
 	Global.debug("new p2 name="+new_player_name)
 	player2_name = new_player_name
-
 
 func _on_settings_player_1_name_gui_input(_event):
 	$"ButtonsGroup/Music Row/DummySpacerOne".hide()
@@ -89,6 +90,9 @@ func _on_settings_player_2_name_mouse_exited():
 	$"ButtonsGroup/Sound Row/DummySpacerTwo".show()
 
 
+func _on_highlight_mode_settings_updated(mode):
+	highlight_mode = mode
+	
 func _on_audio_volume_settings_updated(audio_type, new_audio_volume, new_audio_enabled):
 	Global.debug("audio settings("+str(audio_type)+"): vol="+str(new_audio_volume)+" on/off="+str(new_audio_enabled))
 	if audio_type == Global.AUDIO_TYPE_MUSIC:
@@ -97,3 +101,19 @@ func _on_audio_volume_settings_updated(audio_type, new_audio_volume, new_audio_e
 	else:
 		sound_volume = new_audio_volume
 		sound_enabled = new_audio_enabled
+
+
+func _on_language_0_pressed():
+	Global.debug("lang0 pressed-" +str($"ButtonsGroup/Hints-Language Row/Language-0".button_group))
+	Global.debug("b1 p="+str($"ButtonsGroup/Hints-Language Row/Language-1".button_pressed))
+	Global.debug("b2 p="+str($"ButtonsGroup/Hints-Language Row/Language-2".button_pressed))
+
+func _on_language_1_pressed():
+	Global.debug("lang1 pressed-" +str($"ButtonsGroup/Hints-Language Row/Language-1".button_group))
+	Global.debug("b0 p="+str($"ButtonsGroup/Hints-Language Row/Language-0".button_pressed))
+	Global.debug("b2 p="+str($"ButtonsGroup/Hints-Language Row/Language-2".button_pressed))
+
+func _on_language_2_pressed():
+	Global.debug("lang2 pressed-" +str($"ButtonsGroup/Hints-Language Row/Language-2".button_group))
+	Global.debug("b0 p="+str($"ButtonsGroup/Hints-Language Row/Language-0".button_pressed))
+	Global.debug("b1 p="+str($"ButtonsGroup/Hints-Language Row/Language-1".button_pressed))
