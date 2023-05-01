3b57488118136ac96f72fc602aee08e27c94c05e
InfiniteProductions <justice8@bbox.fr>
2023-05-01 09:28:40 +0200

    Update TheOrigin.gd
    Enable or disable sound effects & music according to settings

--- project/Scripts/TheOrigin.gd
+++ project/Scripts/TheOrigin.gd
@@ -23,13 +23,17 @@ func _ready():
 	musicPlayer.stream = Global.available_musics[currentMusic]
 	
 	print("setting="+str( Global.settings["Audio"]["music"] ))
-	# don't forget: when music enabled => play it !
+
 	if Global.settings["Audio"]["music"] == true:
 		musicPlayer.volume_db = linear_to_db(Global.settings["Audio"]["musicVolume"] / 100.0)
 		musicPlayer.play()
-	
+	else:
+		musicPlayer.stop()
+
 	if Global.settings["Audio"]["soundEffects"] == true:
 		soundEffectPlayer.volume_db = linear_to_db(Global.settings["Audio"]["soundVolume"] / 100.0)
+	else:
+		soundEffectPlayer.stop()
 
 	menu = mainMenuScene.instantiate()
 	game = gameScene.instantiate()
@@ -39,19 +43,25 @@ func _ready():
 	menu.connect("launch_game", _on_game_launched)
 
 
+# no connected for now ?
 func _on_audio_settings_updated(new_music_volume : int, new_sound_volume: int) -> void:
 	musicPlayer.volume_db = linear_to_db(new_music_volume / 100.0)
 	soundEffectPlayer.volume_db = linear_to_db(new_sound_volume / 100.0)
+
 
 func _on_game_launched(gameType : String):
 	Global.debug("launch game="+gameType)
 	Global.previous_scene = get_tree().current_scene.scene_file_path
 	Global.debug("prev scene=" + Global.previous_scene)
+	
 	remove_child(menu)
 	add_child(game)
+	
 	game.connect("game_end", _on_game_is_over)
-	game.connect("tile_put_on_the_board", _on_game_play_tile_soundeffect)
-	game.connect("fourways", _on_game_fourways_soundeffect)
+	
+	if Global.settings["Audio"]["soundEffects"] == true:
+		game.connect("tile_put_on_the_board", _on_game_play_tile_soundeffect)
+		game.connect("fourways", _on_game_fourways_soundeffect)
 	#add gametype, as param,  global or to game
 	#	current_session_data
 
@@ -63,8 +73,10 @@ func _on_game_is_over(status : int):
 		#soundEffectPlayer.play()
 	elif status == Global.GAME_EXIT_STATUS.GAME_LOSS:
 		Global.debug("Game loss")
-		soundEffectPlayer.stream = Global.sound_effects["loss"] #A B variant = random
-		soundEffectPlayer.play()
+		
+		if Global.settings["Audio"]["soundEffects"] == true:
+			soundEffectPlayer.stream = Global.sound_effects["loss"] #A B variant = random
+			soundEffectPlayer.play()
 	elif status == Global.GAME_EXIT_STATUS.USER_QUIT:
 		Global.debug("quit")
 
