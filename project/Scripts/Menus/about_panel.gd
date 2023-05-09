7f4734823bdc29c7a0d1737ff89c89f7f1dcc169
InfiniteProductions <justice8@bbox.fr>
2023-05-09 11:09:41 +0200

    Update about_panel.gd
    use own debug function instead of print

--- project/Scripts/Menus/about_panel.gd
+++ project/Scripts/Menus/about_panel.gd
@@ -3,24 +3,24 @@ extends Control
 var linkOpened : bool = false
 
 func _on_texture_button_pressed():
-	print("textbutton pressed")
+	Global.debug("textbutton pressed")
 	if !linkOpened:
-		print("hide panel")
+		Global.debug("hide panel")
 		hide()
 
 
 func _on_rich_text_label_meta_clicked(meta):
 	var err = OS.shell_open(meta)
 	if err == OK:
-		print("Opened link '%s' successfully!" % meta)
+		Global.debug("Opened link '%s' successfully!" % meta)
 		linkOpened = true
 	else:
-		print("Failed opening the link '%s'!" % meta)
+		Global.debug("Failed opening the link '%s'!" % meta)
 	linkOpened = false
 
 
 func _on_action_button_pressed():
-	print("textbutton pressed lo="+str(linkOpened))
+	Global.debug("textbutton pressed lo="+str(linkOpened))
 	if !linkOpened:
-		print("hide panel")
+		Global.debug("hide panel")
 		hide()
