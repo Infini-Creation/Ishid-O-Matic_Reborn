034b0f314434e29ee62fc542a680907150ca5133
InfiniteProductions <justice8@bbox.fr>
2023-06-08 09:07:42 +0200

    game tournament panel updated
    -add a way to enter our own seed when creating a new tournament

--- project/Scripts/game_tournament_panel.gd
+++ project/Scripts/game_tournament_panel.gd
@@ -2,26 +2,35 @@ extends Control
 
 signal continue_tournament(cont : bool)
 signal back_to_menu
-const default_label : String = "Do you want to continue the current tournament or to start a new one ?"
+@onready var seedPanel = $CenterContainer/TextureRect2/VBoxContainerB
 
-#label setup thing is useless
 func _ready():
-	$CenterContainer/TextureRect2/VBoxContainer/Label.text = default_label
-
-func setup_label(labelToUse : String)  -> void:
-	if labelToUse != null and labelToUse != "":
-		$CenterContainer/TextureRect2/VBoxContainer/Label.text = labelToUse
-	else:
-		$CenterContainer/TextureRect2/VBoxContainer/Label.text = default_label
+	pass
 
 func _on_yes_button_pressed():
 	continue_tournament.emit(true)
 	hide()
 
 func _on_no_button_pressed():
+	#continue_tournament.emit(false)
+	$CenterContainer/TextureRect2/VBoxContainerA.hide()
+	seedPanel.show()
+	
+
+func _on_cancel_button_pressed():
+	back_to_menu.emit()
+	hide()
+
+
+func _on_b_ok_button_pressed():
+	var spv = $CenterContainer/TextureRect2/VBoxContainerB/SeedInput2.value
+	print("SPV="+str(spv))
+	Global.tournamentSeed = spv
+	#read spinbox value, check if it's ok then use it as seed
 	continue_tournament.emit(false)
 	hide()
 
-func _on_cancel_button_pressed():
+
+func _on_b_cancel_button_pressed():
 	back_to_menu.emit()
 	hide()
