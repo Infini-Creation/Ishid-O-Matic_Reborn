9cbaab844f8ec2a0f17044618959f328dcc0d5e1
Infini-Creation <infini.creation.france@gmail.com>
2023-12-21 11:43:38 +0100

    Update NextTileDisplay.gd
    use new tile design, get rid of colour change by code

--- project/Scripts/NextTileDisplay.gd
+++ project/Scripts/NextTileDisplay.gd
@@ -2,5 +2,4 @@ extends TextureRect
 
 func _on_grid_preview_tile(tile : Node2D, color : Color):
 	if tile != null:
-		texture = tile.get_node("Sprite2D").texture
-		modulate = color
+		texture = tile.get_node("Symbol").texture
