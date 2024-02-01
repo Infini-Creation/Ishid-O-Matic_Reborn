3f8b8c67201f8d6811392cb9267e59a1badbfb6e
Infini-Creation <infini.creation.france@gmail.com>
2024-02-01 10:01:48 +0100

    Update final_score_panel.gd
    -use score data provided by game (as array)
    -correctly manage number of players
    -add an increment to make score increase smoothly and faster and faster to its final value

--- project/Scripts/final_score_panel.gd
+++ project/Scripts/final_score_panel.gd
@@ -15,12 +15,14 @@ var initialized : bool = false
 var frameCount : int = 0
 var scoresProcessed : Array = [false, false]
 var currentScoreIndex : int = 0
+var incr : float = 0.0
 
 # should be array, as in game, for both players
 # anim P1, then P2
-func setup(base_score : int, four_ways : int, tiles_remaining : int) -> void:
-	baseScore = base_score
-	fourWays = four_ways
+func setup(base_score : Array, four_ways : Array, tiles_remaining : int) -> void:
+	#need to set up two players data
+	baseScore = base_score[0]
+	fourWays = four_ways[0]
 	tilesRemaining = tiles_remaining
 	initialized = true
 
@@ -28,7 +30,11 @@ func setup(base_score : int, four_ways : int, tiles_remaining : int) -> void:
 # with sounds & maybe shaking
 func _ready():
 	#to remove
-	Global.debug("final score panel ready")
+	Global.debug("final score panel ready nbp="+str(numberOfPlayers))
+
+
+#func debug_players():
+#	Global.debug("----DEBUG---FSP--- nbp="+str(numberOfPlayers))
 
 
 func _process(delta : float):
@@ -60,11 +66,15 @@ func _process(delta : float):
 func process_one_score(label: Node, delta : float) -> void:
 	Global.debug("s="+str(score)+" bs="+str(baseScore)+"  d="+str(delta))
 	prevScore = score
-	score = lerp(score, baseScore, 0.012)	#0.22 base
+	score = lerp(score, baseScore, 0.0012 + incr)	#0.22 base
 	
+	Global.debug("s="+str(score)+" ps="+str(prevScore))
 	if prevScore == score:
-		Global.debug("ps==s, lerp 1")
-		score = lerp(score, baseScore, 1)
+		incr += 0.05 #still too fast for low score
+		Global.debug("ps==s, increment="+str(incr))
+		#score = lerp(score, baseScore, 0.22)
+		#in fact each time ps=s => increase weight by a given increment up to 1
+		
 		
 	if score == baseScore:
 		Global.debug("final score value reached")
@@ -90,9 +100,14 @@ func _on_button_pressed():
 
 
 # to remove
-#func _on_visibility_changed():
-	#Global.debug("final score panel vis changed:"+str(visible))
+func _on_visibility_changed():
+	Global.debug("final score panel vis changed:"+str(visible))
+	if visible == true:
+		if numberOfPlayers == 1:
+			Global.debug("P1: hide P2 line")
+			$CenterContainer/TextureRect2/VBoxContainer/HBoxContainerP2.hide()
+		#HERE: display score anim (setup called by game=>init=>"anim")
 
 # ~unused
 func _on_v_box_container_item_rect_changed():
-	pass # Replace with function body.
+	pass
