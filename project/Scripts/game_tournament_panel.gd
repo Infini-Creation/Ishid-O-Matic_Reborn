83951593f5f8a05d592c0aa5c28afedd0fb41be3
Infini-Creation <infini.creation.france@gmail.com>
2024-03-11 18:05:26 +0100

    tournament subpanel improved a bit
    add a cancel button in addition to define a seed or pick a random one

--- project/Scripts/game_tournament_panel.gd
+++ project/Scripts/game_tournament_panel.gd
@@ -5,6 +5,7 @@ signal back_to_menu
 @onready var seedPanel = $CenterContainer/TextureRect2/VBoxContainerB
 
 func _ready():
+	Global.debug("GTP: transloc="+TranslationServer.get_locale())
 	pass
 
 func _on_yes_button_pressed():
@@ -30,7 +31,14 @@ func _on_b_ok_button_pressed():
 	continue_tournament.emit(false)
 	hide()
 
-
+#?? need to generate a new seed in this case
 func _on_b_cancel_button_pressed():
+	back_to_menu.emit()
+	hide()
+
+
+func _on_b_random_button_pressed() -> void:
+	Global.tournamentSeed = 0
+	continue_tournament.emit(false)
 	back_to_menu.emit()
 	hide()
