6f28fa97e62b3c120ba78d32224a37c504c01631
Infini-Creation <infini.creation.france@gmail.com>
2024-04-12 12:00:41 +0200

    update with translation support
    temporary backup commit

--- project/Scripts/Menus/about_panel.gd
+++ project/Scripts/Menus/about_panel.gd
@@ -1,9 +1,35 @@
 extends Control
 
 var linkOpened : bool = false
+var aboutText : String ## = "[center]Translation Error ![/center]"
+
+
+func _init():
+	Global.debug("about: init called, at="+aboutText)
+
+func _notification(what: int) -> void:
+	Global.debug("about: _notification called, at="+aboutText.substr(100)+"  what="+str(what))
+	
+func _ready():	#or only when visible, ~the 1st time ??
+	Global.debug("about: ready called, at="+aboutText)
+	#if Global.lp_translations.has("ABOUT"):
+		#if Global.lp_translations["ABOUT"].has(TranslationServer.get_locale()):
+			#Global.debug("text is avail in this lang: " +TranslationServer.get_locale())
+			#Global.debug("nb page: "+str(Global.lp_translations["ABOUT"][TranslationServer.get_locale()].size()))
+			#
+			#if Global.lp_translations["ABOUT"][TranslationServer.get_locale()].size() > 0:
+				#aboutText = Global.lp_translations["ABOUT"][TranslationServer.get_locale()][0]
+			#else:
+				#pass
+		#else:
+			#pass
+	#else:
+		#pass
+	#
+	#$CenterContainer/VBoxContainer/TextureButton/RichTextLabel.text = aboutText
 
 func _on_texture_button_pressed():
-	Global.debug("textbutton pressed")
+	Global.debug("about: textbutton pressed")
 	if !linkOpened:
 		Global.debug("hide panel")
 		hide()
@@ -12,20 +38,47 @@ func _on_texture_button_pressed():
 func _on_rich_text_label_meta_clicked(meta):
 	var err = OS.shell_open(meta)
 	if err == OK:
-		Global.debug("Opened link '%s' successfully!" % meta)
+		Global.debug("about: Opened link '%s' successfully!" % meta)
 		linkOpened = true
 	else:
-		Global.debug("Failed opening the link '%s'!" % meta)
+		Global.debug("about: Failed opening the link '%s'!" % meta)
 	linkOpened = false
 
 
 func _on_action_button_pressed():
-	Global.debug("textbutton pressed lo="+str(linkOpened))
+	Global.debug("about: textbutton pressed lo="+str(linkOpened))
 	if !linkOpened:
 		Global.debug("hide panel")
 		hide()
 
 
 func _on_close_button_pressed():
-	Global.debug("close button pressed")
+	Global.debug("about: close button pressed")
 	hide()
+
+
+func _on_visibility_changed():
+	Global.debug("about: vis chg: at="+aboutText)
+	
+	if Global.translation_setting_updated[Global.TRANSLATION_ABOUT_PAGE] == true:
+		Global.debug("about: vis chg: lang updated")
+		aboutText = "[center]Translation Error ![/center]"
+
+		if Global.lp_translations.has(Global.TRANSLATION_ABOUT_PAGE):
+			if Global.lp_translations[Global.TRANSLATION_ABOUT_PAGE].has(TranslationServer.get_locale()):
+				Global.debug("about: lang is avail: " +TranslationServer.get_locale())
+				Global.debug("about: nb page: "+str(Global.lp_translations[Global.TRANSLATION_ABOUT_PAGE][TranslationServer.get_locale()].size()))
+				
+				if Global.lp_translations[Global.TRANSLATION_ABOUT_PAGE][TranslationServer.get_locale()].size() > 0:
+					aboutText = Global.lp_translations[Global.TRANSLATION_ABOUT_PAGE][TranslationServer.get_locale()][0]
+					Global.translation_setting_updated[Global.TRANSLATION_ABOUT_PAGE] = false
+					Global.debug("about: vis chg: lang updated well, flag reset")
+				else:
+					pass
+			else:
+				pass
+		else:
+			pass
+
+	Global.debug("about: vis chg end, at="+aboutText)
+	$CenterContainer/VBoxContainer/TextureButton/RichTextLabel.text = aboutText
--- project/Scripts/help_panel.gd
+++ project/Scripts/help_panel.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const PAGESCOUNT : int = 7
+
 @onready var PagesHolder : Array = [
 	$CenterContainer/VBoxContainer/CenterContainer/Page1,
 	$CenterContainer/VBoxContainer/CenterContainer/Page2,
@@ -15,14 +17,39 @@ signal help_panel_closed
 var current_page : int = 0
 var previous_page : int = 0
 var max_pages : int = 0
+var helpText : String = "[center]Translation Error ![/center]"
 
 func _ready():
 	Global.debug("HelpPanel: _ready call")
 	max_pages = PagesHolder.size() - 1
+	
+	#fill the array as it should be in global/origin, no tests here
+	if Global.lp_translations.has(Global.TRANSLATION_HELP_PAGE):
+		if Global.lp_translations[Global.TRANSLATION_HELP_PAGE].has(TranslationServer.get_locale()):
+			Global.debug("lang is avail: " +TranslationServer.get_locale())
+			Global.debug("nb page: "+str(Global.lp_translations[Global.TRANSLATION_HELP_PAGE][TranslationServer.get_locale()].size()))
+			
+			if Global.lp_translations[Global.TRANSLATION_HELP_PAGE][TranslationServer.get_locale()].size() > 0:
+				for pageIdx in range(0, 7):
+					helpText = Global.lp_translations[Global.TRANSLATION_HELP_PAGE][TranslationServer.get_locale()][pageIdx]
+					Global.debug("pidx=["+str(pageIdx)+"] t=["+helpText+"]")
+					PagesHolder[pageIdx].text = helpText
+			else:
+				pass
+		else:
+			pass
+	else:
+		pass
 
 func update_page(prevPage : int, nextPage : int) -> void:
 	Global.debug("HelpPanel: update page= " + str(prevPage) + " -> " + str(nextPage))
 	PagesHolder[prevPage].hide()
+	#bad this way, need to init translation somewhere, fill misisng data with non fatal stuff
+	if Global.lp_translations["HELP"][TranslationServer.get_locale()].size() > 0:
+		# also check page count idx
+		PagesHolder[nextPage].text = Global.lp_translations["HELP"][TranslationServer.get_locale()][nextPage]
+	else:
+		PagesHolder[nextPage].text = Global.TRANSLATION_ERROR_MESSAGE
 	PagesHolder[nextPage].show()
 
 # to remove eventually
@@ -51,3 +78,46 @@ func _on_close_button_pressed():
 	Global.debug("HelpPanel: button pressed, close it")
 	help_panel_closed.emit()	#is this useful ?
 	hide()
+
+
+func _on_visibility_changed():
+	Global.debug("help vis chg")
+	
+	if visible == false:
+		if current_page != 0:
+			Global.debug("help: reset curr page")
+			current_page = 0
+			#update_page(0, 1) #not enough !!
+			for pageIdx in range(0, max_pages + 1):
+				PagesHolder[pageIdx].hide()
+			PagesHolder[0].show()
+		#doing this way make old page stay behind new one !!
+		
+	#HERE==> refresh tr IF NEEDED
+	
+	#var pageIdx : int = 1
+	#var nodePath : String = "CenterContainer/VBoxContainer/CenterContainer/Page"
+#
+	Global.debug("help: upd tr ??") #YES!! IF settings has changed !
+	#if Global.lp_translations.has("HELP"):
+		#if Global.lp_translations["HELP"].has(TranslationServer.get_locale()):
+			#Global.debug("lang is avail: " +TranslationServer.get_locale())
+			#Global.debug("nb page: "+str(Global.lp_translations["HELP"][TranslationServer.get_locale()].size()))
+			#
+			##or here only tr currentp age but issue, panel need to be closed/opened
+			## or in page browsing, using sub func
+			#if Global.lp_translations["HELP"][TranslationServer.get_locale()].size() > 0:
+				#for pageIdx in range(1, 8):
+					#helpText = Global.lp_translations["HELP"][TranslationServer.get_locale()][pageIdx - 1]
+					#nodePath += str(pageIdx) #+ ".text"
+					#Global.debug("nodep=["+nodePath+"] t=["+helpText+"]")
+					#PagesHolder[pageIdx].text = helpText
+					##$CenterContainer/VBoxContainer/CenterContainer/Page+str(pageIdx).text = helpText
+			#else:
+				#pass
+		#else:
+			#pass
+	#else:
+		#pass
+
+	pass
